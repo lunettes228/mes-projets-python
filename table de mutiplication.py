ce647235731e085fcba830c3e5d9de6bfb075018
lunettes228 <sodjisamuel89@gmail.com>
2022-04-15 09:10:04 +0000

    main

--- table de mutiplication.py
+++ table de mutiplication.py
@@ -4,7 +4,7 @@ k=1
 for mult in range(0,12):
     try:
         x=int(x)
-        print(f"{x}*{k} = {x*k}")
+        print(f"{x} x {k} = {x*k}")
         k=k+1
     except:
         print("ERREUR ENTREZ UN CHIFFRE PAS UNE LETTRE !!! \nREDEMARREZ LE PROGRAMME!!!!!!!!!"  )
