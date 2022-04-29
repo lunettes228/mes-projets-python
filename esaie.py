e7818f2cb53dad6bb6ab319d6f189f76675302e5
lunettes228 <sodjisamuel89@gmail.com>
2022-04-29 06:31:10 +0000

    esaie3

--- esaie.py
+++ esaie.py
@@ -1,15 +1,32 @@
 
-# noms = ["samuel", "roger", "naomi", "v48itali", "roger", "ahlin", "youssef", "rao47ul", "ibrahim", "naffi", "ivan", "zaynab", "kola", "emeraude", "appo",
-#         "merveille", "victo45rine", "victoire", "jédéon", "samira", "walter", "essor", "abdou", "josé55", "raymond", "staff", "happiness"]
+# # noms = ["samuel", "roger", "naomi", "v48itali", "roger", "ahlin", "youssef", "rao47ul", "ibrahim", "naffi", "ivan", "zaynab", "kola", "emeraude", "appo",
+# #         "merveille", "victo45rine", "victoire", "jédéon", "samira", "walter", "essor", "abdou", "josé55", "raymond", "staff", "happiness"]
 
-# ceci c'est juste pour tester la fonction is digit
+# # ceci c'est juste pour tester la fonction is digit
 
-k=input("quel est le nom? ")
-y=any([ x.isdigit() for x in k ])
-while y is True:
-    print("la liste contient un élément avec un chiffre")
-    break
-else:
-    print("la liste ne contient pas d'élément avec un chiffre")
+# k=input("quel est le nom? ")
+# y=any([ x.isdigit() for x in k ])
+# while y is True:
+#     print("la liste contient un élément avec un chiffre")
+#     break
+# else:
+#     print("la liste ne contient pas d'élément avec un chiffre")
     
-# la fonction is digit est maintenant maitrisé
+# # la fonction is digit est maintenant maitrisé
+
+
+
+liste_de_fichiers =["sam.png", "google.com", "zoom.exe", "formatrion_complete.mkv", "dadju_reine.mp3"]
+extension_de_liste=((".exe","exécutable"),  (".com","lien(url)"),(".mkv","vidéo"),(".mp3","audio"),(".png","image"))
+for n in liste_de_fichiers:
+    a=n.split(".")
+    b=".".join(a)
+    for i in extension_de_liste:
+        h=i[0].split(".")
+        if a[-1] in h :
+            print(f"bonjour ce fichier est un: {h[1]}")
+            break
+        else:
+            print("hello")
+            break
+        
