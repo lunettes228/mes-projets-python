6744ffd1708474fbcc1431480311615785df90e8
lunettes228 <sodjisamuel89@gmail.com>
2022-04-30 10:09:36 +0000

    main

--- bb.py
+++ bb.py
@@ -1,5 +1,9 @@
-x = input("quel mot ou nombre vous voulez entrer???")
-if x.isdigit():
-    print("oui il est digit")
-else:
-    print("non is not digit")
+# x = input("quel mot ou nombre vous voulez entrer???")
+# if x.isdigit():
+#     print("oui il est digit")
+# else:
+#     print("non is not digit")
+
+x=".join"
+y=x.split(".")
+print(y)
--- esaie.py
+++ esaie.py
@@ -11,22 +11,38 @@
 #     break
 # else:
 #     print("la liste ne contient pas d'élément avec un chiffre")
-    
+
 # # la fonction is digit est maintenant maitrisé
 
 
+# liste_de_fichiers = ["sam.png", "google.com", "zoom.exe",
+#                      "formatrion_complete.mkv", "dadju_reine.mp3"]
+# extension_de_liste = (("exe", "exécutable"),  ("com", "lien(url)"),
+#                       ("mkv", "vidéo"), ("mp3", "audio"), ("png", "image"))
+# for n in liste_de_fichiers:
+#     a = n.split(".")
 
-liste_de_fichiers =["sam.png", "google.com", "zoom.exe", "formatrion_complete.mkv", "dadju_reine.mp3"]
-extension_de_liste=((".exe","exécutable"),  (".com","lien(url)"),(".mkv","vidéo"),(".mp3","audio"),(".png","image"))
-for n in liste_de_fichiers:
-    a=n.split(".")
-    b=".".join(a)
-    for i in extension_de_liste:
-        h=i[0].split(".")
-        if a[-1] in h :
-            print(f"bonjour ce fichier est un: {h[1]}")
-            break
-        else:
-            print("hello")
-            break
-        
+#     b = ".".join(a)
+#     for i in extension_de_liste:
+#         for m in i:
+#             h = i[0].split(".")
+#             if a[-1] == i[0]:
+#                 print(f"bonjour ce fichier est un/une: {m}")
+#                 break
+#             else:
+#                 print("hello")
+#                 break
+
+#     break
+
+noms = ["samuel", "ahlin", "mike", "roger", "merveille"]
+
+
+noms_et_age = {"samuel": "18 ans", "ahlin": "28 ans",
+               "mike": "14 ans", "roger": "22 ans", "merveille": "6 ans"}
+
+for i in noms:
+    for n in noms_et_age:
+        p = n[i]
+        if p in n:
+            print(f"le nom de la personne est {i} : il a {p} ans")
