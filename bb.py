b4f771d145a64d5c465aa9f614a25e61e0bca98d
lunettes228 <sodjisamuel89@gmail.com>
2022-05-14 17:43:13 +0000

    main

--- bb.py
+++ bb.py
@@ -4,18 +4,17 @@ class personne:
         self.age=age
     def se_presenter(self):
         print(f"vous vous appelez : {self.nom}, vous avez : {self.age} ans".capitalize())
+        self.est_majeur()
     def est_majeur(self):
-        return self.age>=18
-            # print(f"{self.nom} vous etes majeur !!!!".capitalize())
-        #     return True
-        # else:
-            # print(f"{self.nom} vous n'estes pas majeur !!!".capitalize())
+        if self.age>=18:
+            print(f"moi {self.nom} je suis majeur !!!!".capitalize())
+            # return True
+        else:
+            print(f"moi {self.nom} je suis  mineur !!!".capitalize())
             # return False
             
         
 personne1=personne("jean", 30)
 personne2=personne("paul", 15)
 personne1.se_presenter()
-print(f"est majeur1: {personne1.est_majeur()}".capitalize())
 personne2.se_presenter()
-print(f"est majeur 2: {personne2.est_majeur()}".capitalize())
