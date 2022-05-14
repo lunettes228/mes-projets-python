f0901cf2c32479a745053d4215e40d0a3df10389
lunettes228 <sodjisamuel89@gmail.com>
2022-05-14 16:41:49 +0000

    main

--- bb.py
+++ bb.py
@@ -1,9 +1,11 @@
-# x = input("quel mot ou nombre vous voulez entrer???")
-# if x.isdigit():
-#     print("oui il est digit")
-# else:
-#     print("non is not digit")
-
-x=".join"
-y=x.split(".")
-print(y)
+class personne:
+    def __init__(self,nom,age):
+        self.nom=nom
+        self.age=int(age)
+    def se_presenter(self):
+        print(f"vous vous appelez : {self.nom}, vous avez : {self.age} ans".capitalize())
+        
+personne1=personne("jean", 30)
+personne2=personne("paul", 25)
+personne1.se_presenter()
+personne2.se_presenter()
